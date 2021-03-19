a9cde44aa8771801bfd7f902687f908c655db528
BartWesthoff-HRO <60735088+BartWesthoff-HRO@users.noreply.github.com>
2021-03-19 10:36:11 +0100

    Add project files.

---
+++ Beheer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CinemaProjectB
+{
+    class Beheer
+    {
+        public static void Clear()
+        {
+            Console.Clear();           
+        }
+        public static string Input(string vraag = null)
+        {
+            if (vraag != null)
+            {
+                Console.WriteLine(vraag);
+            }
+            
+            return Console.ReadLine();
+        }
+        public static void CreateAccount()
+        {
+            string naam = Input("Wat is je naam?: ");
+            int Leetijd = int.Parse(Input("Wat is je leeftijd?: "));
+
+            Console.WriteLine($"Naam {naam} leeftijd: {Leetijd}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CinemaProjectB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Beheer.Clear();
+            //string naam = Beheer.Input("Wat is je naam?: ");
+            //Console.WriteLine(naam);
+            //Beheer.CreateAccount();
+            MainMenu();
+        }
+
+        public static void MainMenu()
+        {
+            string prompt = "Welkom bij de Bioscoop";
+            string[] options = { "Login", "Registreren" };
+            Menu StartPagina = new Menu(prompt, options);
+            StartPagina.DisplayOptions();
+            int selectedIndex = StartPagina.Run();
+
+            if (selectedIndex == 0)
+            {
+                Login();
+            }
+            else if (selectedIndex == 1)
+            {
+                Registreren();
+            }
+        }
+        public static void Login()
+        {
+            Console.WriteLine("Ingelogd");
+            Console.ReadKey(true);
+            MainMenu();
+        }
+        public static void Registreren()
+        {
+            Console.WriteLine("Registeren");
+            Console.ReadKey(true);
+            MainMenu();
+        }
+    }
+}
